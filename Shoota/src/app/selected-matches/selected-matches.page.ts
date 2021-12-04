881353d42dc4b1d28ba47f3f587bd69a7b39bd6f
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-12-04 21:15:36 +0200

    completed navigation

--- Shoota/src/app/selected-matches/selected-matches.page.ts
+++ Shoota/src/app/selected-matches/selected-matches.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-selected-matches',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectedMatchesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+  }
+
+  toStandings(){
+    this.router.navigate(['selected-standings']);
   }
 
 }
