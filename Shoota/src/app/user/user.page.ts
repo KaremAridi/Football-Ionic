aa4888b0185260ac299b3a3cf571d235db934478
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-12-04 20:51:39 +0200

    user page nav

--- Shoota/src/app/user/user.page.ts
+++ Shoota/src/app/user/user.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+  }
+
+  toTeam(){
+    this.router.navigate(['selected-team']);
+  }
+
+  toLeague(){
+    this.router.navigate(['selected-matches']);
   }
 
 }
